2035ca676a7e2c7785bcc2bb71942c7153981cc1
Michi <mbaueretz@googlemail.com>
2021-11-01 09:45:18 +0100

    add emojis for special days to good morning tweet

--- calendar/tweet.py
+++ calendar/tweet.py
@@ -23,6 +23,37 @@ months = [
 ]
 
 
+def is_valentines_day(month, day):
+    return month == 2 and day == 14
+
+
+def is_easter(year, month, day):
+    # see Astronomical Algorithms by Jean Meeus
+    a = year % 19
+    b = year // 100
+    c = year % 100
+    d = b // 4
+    e = b % 4
+    f = (b + 8) // 25
+    g = (b - f + 1) // 3
+    h = (19 * a + b - d - g + 15) % 30
+    i = c // 4
+    k = c % 4
+    l = (32 + 2 * e + 2 * i - h - k) % 7
+    m = (a + 11 * h + 22 * l) // 451
+    n = (h + l - 7 * m + 114) // 31
+    p = (h + l - 7 * m + 114) % 31
+    return month == n and day == p + 1
+
+
+def is_halloween(month, day):
+    return month == 10 and day == 31
+
+
+def is_christmas(month, day):
+    return month == 12 and day == 25
+
+
 today = date.today()
 year = today.year
 month = today.month
@@ -46,7 +77,18 @@ api = Api(
     consumer_key=consumer_key,
     consumer_secret=consumer_secret
 )
-message = "Guten Morgen.🌞\n\n"
+message = "Guten Morgen."
+if is_valentines_day(month, day):
+    message += "💞"
+elif is_easter(year, month, day):
+    message += "🐰"
+elif is_halloween(month, day):
+    message += "🎃"
+elif is_christmas(month, day):
+    message += "🎅"
+else:
+    message += "🌞"
+message += "\n\n"
 message += "%s (greg.)\n" % gregorian_date
 message += "%s (jul./a.u.c.)\n" % julian_date
 message += "%s (jüd.)\n" % hebrew_date
@@ -55,8 +97,7 @@ message += "%s (frz.)²\n" % french_date
 message += "%s (Maya)³" % mayan_date
 result = api.PostUpdate(message)
 id = result.id
-message = "¹Microsofts kuwaitischer Algorithmus, "
-message += "kann vom astronomisch bestimmten Datum abweichen.\n"
+message = "¹Kuwaitischer Algorithmus\n"
 message += "²Romme-Schaltregel\n"
 message += "³GMT-Korrelation"
 api.PostUpdate(message, in_reply_to_status_id=id)
