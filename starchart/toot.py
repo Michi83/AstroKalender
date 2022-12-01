b90e308ee12052d341d59a1c48f22dcc9e373405
Michi <mbaueretz@googlemail.com>
2022-12-01 06:25:31 +0100

    implement image descriptions in mastodon

--- starchart/toot.py
+++ starchart/toot.py
@@ -22,17 +22,27 @@ months = [
 ]
 
 
+def get_alt_text(date_string):
+    with open("alttexts.txt", "r") as file:
+        for line in file:
+            if line[:10] == date_string:
+                return line[11:].strip()
+    return ""
+
+
 today = date.today()
 year = today.year
 month = today.month
 day = today.day
-filename = "img/%04d-%02d-%02d.png" % (year, month, day)
-message = "Der Sternenhimmel am %d. %s.\n" % (day, months[month])
+date_string = "%04d-%02d-%02d" % (year, month, day)
+filename = "img/%s.png" % date_string
+alt_text = get_alt_text(date_string)
+message = "Der Sternenhimmel am %d. %s %d.\n" % (day, months[month], year)
 message += "(50° N, 10° O, 22 Uhr MEZ)\n"
 message += "#ClearSkies! #Astronomie #Astrodon"
 mastodon = Mastodon(
     access_token=mastodon_access_token,
     api_base_url=mastodon_api_base_url
 )
-media_id = mastodon.media_post(filename).id
+media_id = mastodon.media_post(filename, description=alt_text).id
 mastodon.status_post(message, media_ids=[media_id])
