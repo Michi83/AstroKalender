d60dbaf62e652e30379f094040a8b71178174365
Michi <mbaueretz@googlemail.com>
2023-01-27 19:03:17 +0100

    improve daily solar events

--- sun/toot.py
+++ sun/toot.py
@@ -2,7 +2,7 @@ from sys import path
 path.append("..")
 from config import mastodon_access_token, mastodon_api_base_url
 from datetime import date, datetime, timedelta
-from math import asin, atan2, cos, pi, radians as rad, sin, tan
+from math import asin, atan2, cos, floor, pi, radians as rad, sin, tan
 from pytz import timezone, utc
 from mastodon import Mastodon
 
@@ -35,7 +35,7 @@ class Datum:
 
 def load_data(filename):
     global data
-    data = []
+    data = {}
     with open(filename, "r") as file:
         for line in file:
             if line == "$$SOE\n":
@@ -49,7 +49,8 @@ def load_data(filename):
             delta = rad(float(line[51:60]))
             theta = rad(15 * float(line[62:75]))
             datum = Datum(date, jdn, alpha, delta, theta)
-            data.append(datum)
+            data[date] = datum
+            data[jdn] = datum
 
 
 def interpolate(x, x1, x2, y1, y2):
@@ -58,25 +59,25 @@ def interpolate(x, x1, x2, y1, y2):
 
 def right_ascension_declination_sidereal_time(jdn):
     x = jdn
-    for i in range(len(data)):
-        if data[i].jdn < x and data[i + 1].jdn >= x:
-            x1 = data[i].jdn
-            x2 = data[i + 1].jdn
-            y1 = data[i].alpha
-            y2 = data[i + 1].alpha
-            if y2 < y1:
-                y2 += 2 * pi
-            alpha = interpolate(x, x1, x2, y1, y2)
-            y1 = data[i].delta
-            y2 = data[i + 1].delta
-            delta = interpolate(x, x1, x2, y1, y2)
-            y1 = data[i].theta
-            y2 = data[i + 1].theta
-            if y2 < y1:
-                y2 += 2 * pi
-            y2 += 2 * pi
-            theta = interpolate(x, x1, x2, y1, y2)
-            return alpha, delta, theta
+    datum1 = data[floor(jdn - 0.5) + 0.5]
+    datum2 = data[floor(jdn - 0.5) + 1.5]
+    x1 = datum1.jdn
+    x2 = datum2.jdn
+    y1 = datum1.alpha
+    y2 = datum2.alpha
+    if y2 < y1:
+        y2 += 2 * pi
+    alpha = interpolate(x, x1, x2, y1, y2)
+    y1 = datum1.delta
+    y2 = datum2.delta
+    delta = interpolate(x, x1, x2, y1, y2)
+    y1 = datum1.theta
+    y2 = datum2.theta
+    if y2 < y1:
+        y2 += 2 * pi
+    y2 += 2 * pi
+    theta = interpolate(x, x1, x2, y1, y2)
+    return alpha, delta, theta
 
 
 def azimuth_altitude(jdn, phi):
@@ -101,12 +102,9 @@ def toot(city, phi, tz, filename):
         months_de[1] = "Jänner"
     date_en = "%4i-%s-%02i" % (year, months_en[month], day)
     date_de = "%i. %s %i" % (day, months_de[month], year)
-    for datum in data:
-        if datum.date == date_en:
-            jdn = datum.jdn
-            break
+    jdn = data[date_en].jdn
     message = "#GutenMorgen.🌞\n\n"
-    message += "Der Sonnenlauf am %s in %s:\n" % (date_de, city)
+    message += "Der Sonnenlauf am %s in #%s:\n" % (date_de, city)
     for h in range(-2, 23):
         for m in range(60):
             if h < 0:
@@ -146,8 +144,7 @@ def toot(city, phi, tz, filename):
             if a1 >= rad(-18) and a2 < rad(-18):
                 message += ("%02i:%02i Ende der astronomischen Dämmerung\n" %
                     (dt_local.hour, dt_local.minute))
-    message += "\n"
-    message += "#Astronomie #Astrokalender #Astrodon"
+    message += "\n#Astronomie #AstroKalender #Astrodon"
     mastodon = Mastodon(
         access_token=mastodon_access_token,
         api_base_url=mastodon_api_base_url
